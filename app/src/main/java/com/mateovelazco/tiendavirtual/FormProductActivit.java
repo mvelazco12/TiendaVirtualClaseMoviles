f548a04b94ccf4f74191dc02d21bc1c1f0177c5a
Mateo Velazco <mvelazco661@unab.edu.co>
2024-03-06 17:00:52 -0500

    commit pls

---
+++ app/src/main/java/com/mateovelazco/tiendavirtual/FormProductActivit.java
@@ -0,0 +1,14 @@
+package com.mateovelazco.tiendavirtual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormProductActivit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_product);
+    }
+}
